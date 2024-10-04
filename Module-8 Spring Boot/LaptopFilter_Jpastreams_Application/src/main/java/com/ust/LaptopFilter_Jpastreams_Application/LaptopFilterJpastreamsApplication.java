10caf23d6e36f01cb232cd3f25ef4462453b63d6
Parthasaradhi H <parthasaradhi.h@gmail.com>
2024-10-04 12:34:47 +0530

    Added Assignment Laptop price filtering

---
+++ Module-8 Spring Boot/LaptopFilter_Jpastreams_Application/src/main/java/com/ust/LaptopFilter_Jpastreams_Application/LaptopFilterJpastreamsApplication.java
@@ -0,0 +1,13 @@
+package com.ust.LaptopFilter_Jpastreams_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopFilterJpastreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopFilterJpastreamsApplication.class, args);
+	}
+
+}
---
+++ Module-8 Spring Boot/LaptopFilter_Jpastreams_Application/src/test/java/com/ust/LaptopFilter_Jpastreams_Application/LaptopFilterJpastreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.LaptopFilter_Jpastreams_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopFilterJpastreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
